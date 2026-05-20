608b622a594598ae60c2124a3553f67c65d23061
Latias94 <superfrankie621@gmail.com>
2026-05-20 12:45:46 +0800

    refactor: deepen json repair scanner

--- packages/llm_dart_ai/lib/src/common/json_repair.dart
+++ packages/llm_dart_ai/lib/src/common/json_repair.dart
@@ -1,17 +1,95 @@
 String fixJson(String input) {
-  final stack = <_JsonScannerState>[_JsonScannerState.root];
-  var lastValidIndex = -1;
-  int? literalStart;
+  return _JsonRepairScanner(input).repair();
+}
 
-  void processValueStart(
+final class _JsonRepairScanner {
+  final String input;
+  final List<_JsonScannerState> _stack = [_JsonScannerState.root];
+  var _lastValidIndex = -1;
+  int? _literalStart;
+
+  _JsonRepairScanner(this.input);
+
+  String repair() {
+    for (var index = 0; index < input.length; index++) {
+      _processCharacter(input[index], index);
+    }
+
+    final buffer = StringBuffer(input.substring(0, _lastValidIndex + 1));
+    _appendMissingClosures(buffer);
+    return buffer.toString();
+  }
+
+  void _processCharacter(String char, int index) {
+    switch (_stack.last) {
+      case _JsonScannerState.root:
+        _processValueStart(char, index, _JsonScannerState.finish);
+        break;
+      case _JsonScannerState.finish:
+        break;
+      case _JsonScannerState.insideObjectStart:
+        _processObjectStart(char, index);
+        break;
+      case _JsonScannerState.insideObjectKey:
+        if (char == '"') {
+          _swapTop(_JsonScannerState.insideObjectAfterKey);
+        }
+        break;
+      case _JsonScannerState.insideObjectAfterKey:
+        if (char == ':') {
+          _swapTop(_JsonScannerState.insideObjectBeforeValue);
+        }
+        break;
+      case _JsonScannerState.insideObjectBeforeValue:
+        _processValueStart(
+          char,
+          index,
+          _JsonScannerState.insideObjectAfterValue,
+        );
+        break;
+      case _JsonScannerState.insideObjectAfterValue:
+        _processAfterObjectValue(char, index);
+        break;
+      case _JsonScannerState.insideObjectAfterComma:
+        if (char == '"') {
+          _swapTop(_JsonScannerState.insideObjectKey);
+        }
+        break;
+      case _JsonScannerState.insideArrayStart:
+        _processArrayStart(char, index);
+        break;
+      case _JsonScannerState.insideArrayAfterValue:
+        _processArrayAfterValue(char, index);
+        break;
+      case _JsonScannerState.insideArrayAfterComma:
+        _processValueStart(
+            char, index, _JsonScannerState.insideArrayAfterValue);
+        break;
+      case _JsonScannerState.insideString:
+        _processString(char, index);
+        break;
+      case _JsonScannerState.insideStringEscape:
+        _stack.removeLast();
+        _lastValidIndex = index;
+        break;
+      case _JsonScannerState.insideNumber:
+        _processNumber(char, index);
+        break;
+      case _JsonScannerState.insideLiteral:
+        _processLiteral(char, index);
+        break;
+    }
+  }
+
+  void _processValueStart(
     String char,
     int index,
     _JsonScannerState nextState,
   ) {
     switch (char) {
       case '"':
-        lastValidIndex = index;
-        stack
+        _lastValidIndex = index;
+        _stack
           ..removeLast()
           ..add(nextState)
           ..add(_JsonScannerState.insideString);
@@ -19,15 +97,15 @@ String fixJson(String input) {
       case 'f':
       case 't':
       case 'n':
-        lastValidIndex = index;
-        literalStart = index;
-        stack
+        _lastValidIndex = index;
+        _literalStart = index;
+        _stack
           ..removeLast()
           ..add(nextState)
           ..add(_JsonScannerState.insideLiteral);
         break;
       case '-':
-        stack
+        _stack
           ..removeLast()
           ..add(nextState)
           ..add(_JsonScannerState.insideNumber);
@@ -42,22 +120,22 @@ String fixJson(String input) {
       case '7':
       case '8':
       case '9':
-        lastValidIndex = index;
-        stack
+        _lastValidIndex = index;
+        _stack
           ..removeLast()
           ..add(nextState)
           ..add(_JsonScannerState.insideNumber);
         break;
       case '{':
-        lastValidIndex = index;
-        stack
+        _lastValidIndex = index;
+        _stack
           ..removeLast()
           ..add(nextState)
           ..add(_JsonScannerState.insideObjectStart);
         break;
       case '[':
-        lastValidIndex = index;
-        stack
+        _lastValidIndex = index;
+        _stack
           ..removeLast()
           ..add(nextState)
           ..add(_JsonScannerState.insideArrayStart);
@@ -65,241 +143,197 @@ String fixJson(String input) {
     }
   }
 
-  void processAfterObjectValue(String char, int index) {
+  void _processObjectStart(String char, int index) {
     switch (char) {
-      case ',':
-        stack
-          ..removeLast()
-          ..add(_JsonScannerState.insideObjectAfterComma);
+      case '"':
+        _swapTop(_JsonScannerState.insideObjectKey);
         break;
       case '}':
-        lastValidIndex = index;
-        stack.removeLast();
+        _lastValidIndex = index;
+        _stack.removeLast();
         break;
     }
   }
 
-  void processAfterArrayValue(String char, int index) {
+  void _processAfterObjectValue(String char, int index) {
     switch (char) {
       case ',':
-        stack
-          ..removeLast()
-          ..add(_JsonScannerState.insideArrayAfterComma);
+        _swapTop(_JsonScannerState.insideObjectAfterComma);
         break;
-      case ']':
-        lastValidIndex = index;
-        stack.removeLast();
+      case '}':
+        _lastValidIndex = index;
+        _stack.removeLast();
         break;
     }
   }
 
-  for (var index = 0; index < input.length; index++) {
-    final char = input[index];
-    final currentState = stack.last;
-
-    switch (currentState) {
-      case _JsonScannerState.root:
-        processValueStart(char, index, _JsonScannerState.finish);
+  void _processArrayStart(String char, int index) {
+    switch (char) {
+      case ']':
+        _lastValidIndex = index;
+        _stack.removeLast();
         break;
-      case _JsonScannerState.finish:
-        break;
-      case _JsonScannerState.insideObjectStart:
-        switch (char) {
-          case '"':
-            stack
-              ..removeLast()
-              ..add(_JsonScannerState.insideObjectKey);
-            break;
-          case '}':
-            lastValidIndex = index;
-            stack.removeLast();
-            break;
-        }
-        break;
-      case _JsonScannerState.insideObjectKey:
-        if (char == '"') {
-          stack
-            ..removeLast()
-            ..add(_JsonScannerState.insideObjectAfterKey);
-        }
-        break;
-      case _JsonScannerState.insideObjectAfterKey:
-        if (char == ':') {
-          stack
-            ..removeLast()
-            ..add(_JsonScannerState.insideObjectBeforeValue);
-        }
-        break;
-      case _JsonScannerState.insideObjectBeforeValue:
-        processValueStart(
+      default:
+        _lastValidIndex = index;
+        _processValueStart(
           char,
           index,
-          _JsonScannerState.insideObjectAfterValue,
+          _JsonScannerState.insideArrayAfterValue,
         );
         break;
-      case _JsonScannerState.insideObjectAfterValue:
-        processAfterObjectValue(char, index);
+    }
+  }
+
+  void _processArrayAfterValue(String char, int index) {
+    switch (char) {
+      case ',':
+        _swapTop(_JsonScannerState.insideArrayAfterComma);
         break;
-      case _JsonScannerState.insideObjectAfterComma:
-        if (char == '"') {
-          stack
-            ..removeLast()
-            ..add(_JsonScannerState.insideObjectKey);
-        }
+      case ']':
+        _lastValidIndex = index;
+        _stack.removeLast();
         break;
-      case _JsonScannerState.insideArrayStart:
-        switch (char) {
-          case ']':
-            lastValidIndex = index;
-            stack.removeLast();
-            break;
-          default:
-            lastValidIndex = index;
-            processValueStart(
-              char,
-              index,
-              _JsonScannerState.insideArrayAfterValue,
-            );
-            break;
-        }
-        break;
-      case _JsonScannerState.insideArrayAfterValue:
-        switch (char) {
-          case ',':
-            stack
-              ..removeLast()
-              ..add(_JsonScannerState.insideArrayAfterComma);
-            break;
-          case ']':
-            lastValidIndex = index;
-            stack.removeLast();
-            break;
-          default:
-            lastValidIndex = index;
-            break;
-        }
-        break;
-      case _JsonScannerState.insideArrayAfterComma:
-        processValueStart(char, index, _JsonScannerState.insideArrayAfterValue);
-        break;
-      case _JsonScannerState.insideString:
-        switch (char) {
-          case '"':
-            stack.removeLast();
-            lastValidIndex = index;
-            break;
-          case '\\':
-            stack.add(_JsonScannerState.insideStringEscape);
-            break;
-          default:
-            lastValidIndex = index;
-            break;
-        }
-        break;
-      case _JsonScannerState.insideStringEscape:
-        stack.removeLast();
-        lastValidIndex = index;
-        break;
-      case _JsonScannerState.insideNumber:
-        switch (char) {
-          case '0':
-          case '1':
-          case '2':
-          case '3':
-          case '4':
-          case '5':
-          case '6':
-          case '7':
-          case '8':
-          case '9':
-            lastValidIndex = index;
-            break;
-          case 'e':
-          case 'E':
-          case '-':
-          case '.':
-            break;
-          case ',':
-            stack.removeLast();
-            if (stack.last == _JsonScannerState.insideArrayAfterValue) {
-              processAfterArrayValue(char, index);
-            }
-            if (stack.last == _JsonScannerState.insideObjectAfterValue) {
-              processAfterObjectValue(char, index);
-            }
-            break;
-          case '}':
-            stack.removeLast();
-            if (stack.last == _JsonScannerState.insideObjectAfterValue) {
-              processAfterObjectValue(char, index);
-            }
-            break;
-          case ']':
-            stack.removeLast();
-            if (stack.last == _JsonScannerState.insideArrayAfterValue) {
-              processAfterArrayValue(char, index);
-            }
-            break;
-          default:
-            stack.removeLast();
-            break;
-        }
-        break;
-      case _JsonScannerState.insideLiteral:
-        final partialLiteral = input.substring(literalStart!, index + 1);
-        if (!_isLiteralPrefix(partialLiteral)) {
-          stack.removeLast();
-          if (stack.last == _JsonScannerState.insideObjectAfterValue) {
-            processAfterObjectValue(char, index);
-          } else if (stack.last == _JsonScannerState.insideArrayAfterValue) {
-            processAfterArrayValue(char, index);
-          }
-        } else {
-          lastValidIndex = index;
-        }
+      default:
+        _lastValidIndex = index;
         break;
     }
   }
 
-  var result = input.substring(0, lastValidIndex + 1);
-
-  for (var index = stack.length - 1; index >= 0; index--) {
-    switch (stack[index]) {
-      case _JsonScannerState.insideString:
-        result += '"';
+  void _processAfterArrayValue(String char, int index) {
+    switch (char) {
+      case ',':
+        _swapTop(_JsonScannerState.insideArrayAfterComma);
         break;
-      case _JsonScannerState.insideObjectKey:
-      case _JsonScannerState.insideObjectAfterKey:
-      case _JsonScannerState.insideObjectStart:
-      case _JsonScannerState.insideObjectBeforeValue:
-      case _JsonScannerState.insideObjectAfterValue:
-      case _JsonScannerState.insideObjectAfterComma:
-        result += '}';
-        break;
-      case _JsonScannerState.insideArrayStart:
-      case _JsonScannerState.insideArrayAfterValue:
-      case _JsonScannerState.insideArrayAfterComma:
-        result += ']';
-        break;
-      case _JsonScannerState.insideLiteral:
-        final partialLiteral = input.substring(literalStart!, input.length);
-        if ('true'.startsWith(partialLiteral)) {
-          result += 'true'.substring(partialLiteral.length);
-        } else if ('false'.startsWith(partialLiteral)) {
-          result += 'false'.substring(partialLiteral.length);
-        } else if ('null'.startsWith(partialLiteral)) {
-          result += 'null'.substring(partialLiteral.length);
-        }
-        break;
-      case _JsonScannerState.root:
-      case _JsonScannerState.finish:
-      case _JsonScannerState.insideStringEscape:
-      case _JsonScannerState.insideNumber:
+      case ']':
+        _lastValidIndex = index;
+        _stack.removeLast();
         break;
     }
   }
 
-  return result;
+  void _processString(String char, int index) {
+    switch (char) {
+      case '"':
+        _stack.removeLast();
+        _lastValidIndex = index;
+        break;
+      case r'\':
+        _stack.add(_JsonScannerState.insideStringEscape);
+        break;
+      default:
+        _lastValidIndex = index;
+        break;
+    }
+  }
+
+  void _processNumber(String char, int index) {
+    switch (char) {
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        _lastValidIndex = index;
+        break;
+      case 'e':
+      case 'E':
+      case '-':
+      case '.':
+        break;
+      case ',':
+        _stack.removeLast();
+        if (_stack.last == _JsonScannerState.insideArrayAfterValue) {
+          _processAfterArrayValue(char, index);
+        }
+        if (_stack.last == _JsonScannerState.insideObjectAfterValue) {
+          _processAfterObjectValue(char, index);
+        }
+        break;
+      case '}':
+        _stack.removeLast();
+        if (_stack.last == _JsonScannerState.insideObjectAfterValue) {
+          _processAfterObjectValue(char, index);
+        }
+        break;
+      case ']':
+        _stack.removeLast();
+        if (_stack.last == _JsonScannerState.insideArrayAfterValue) {
+          _processAfterArrayValue(char, index);
+        }
+        break;
+      default:
+        _stack.removeLast();
+        break;
+    }
+  }
+
+  void _processLiteral(String char, int index) {
+    final partialLiteral = input.substring(_literalStart!, index + 1);
+    if (_isLiteralPrefix(partialLiteral)) {
+      _lastValidIndex = index;
+      return;
+    }
+
+    _stack.removeLast();
+    if (_stack.last == _JsonScannerState.insideObjectAfterValue) {
+      _processAfterObjectValue(char, index);
+    } else if (_stack.last == _JsonScannerState.insideArrayAfterValue) {
+      _processAfterArrayValue(char, index);
+    }
+  }
+
+  void _appendMissingClosures(StringBuffer buffer) {
+    for (var index = _stack.length - 1; index >= 0; index--) {
+      switch (_stack[index]) {
+        case _JsonScannerState.insideString:
+          buffer.write('"');
+          break;
+        case _JsonScannerState.insideObjectKey:
+        case _JsonScannerState.insideObjectAfterKey:
+        case _JsonScannerState.insideObjectStart:
+        case _JsonScannerState.insideObjectBeforeValue:
+        case _JsonScannerState.insideObjectAfterValue:
+        case _JsonScannerState.insideObjectAfterComma:
+          buffer.write('}');
+          break;
+        case _JsonScannerState.insideArrayStart:
+        case _JsonScannerState.insideArrayAfterValue:
+        case _JsonScannerState.insideArrayAfterComma:
+          buffer.write(']');
+          break;
+        case _JsonScannerState.insideLiteral:
+          _appendLiteralSuffix(buffer);
+          break;
+        case _JsonScannerState.root:
+        case _JsonScannerState.finish:
+        case _JsonScannerState.insideStringEscape:
+        case _JsonScannerState.insideNumber:
+          break;
+      }
+    }
+  }
+
+  void _appendLiteralSuffix(StringBuffer buffer) {
+    final partialLiteral = input.substring(_literalStart!, input.length);
+    if ('true'.startsWith(partialLiteral)) {
+      buffer.write('true'.substring(partialLiteral.length));
+    } else if ('false'.startsWith(partialLiteral)) {
+      buffer.write('false'.substring(partialLiteral.length));
+    } else if ('null'.startsWith(partialLiteral)) {
+      buffer.write('null'.substring(partialLiteral.length));
+    }
+  }
+
+  void _swapTop(_JsonScannerState state) {
+    _stack
+      ..removeLast()
+      ..add(state);
+  }
 }
 
 bool _isLiteralPrefix(String value) {
--- packages/llm_dart_ai/test/json_repair_test.dart
+++ packages/llm_dart_ai/test/json_repair_test.dart
@@ -5,6 +5,10 @@ import 'package:test/test.dart';
 
 void main() {
   group('fixJson', () {
+    test('handles empty input', () {
+      expect(fixJson(''), '');
+    });
+
     test('closes nested objects and arrays', () {
       final repaired = fixJson('{"items":[{"name":"alpha"');
 
@@ -24,6 +28,149 @@ void main() {
     test('trims incomplete trailing object keys', () {
       expect(jsonDecode(fixJson('{"ready":true,"dangling"')), {
         'ready': true,
+      });
+    });
+
+    group('Vercel fix-json parity', () {
+      test('repairs partial root literals', () {
+        expect(fixJson('nul'), 'null');
+        expect(fixJson('t'), 'true');
+        expect(fixJson('fals'), 'false');
+      });
+
+      test('repairs incomplete root numbers', () {
+        expect(fixJson('12.'), '12');
+        expect(fixJson('12.2'), '12.2');
+        expect(fixJson('-12'), '-12');
+        expect(fixJson('-'), '');
+        expect(fixJson('2.5e'), '2.5');
+        expect(fixJson('2.5e-'), '2.5');
+        expect(fixJson('2.5e3'), '2.5e3');
+        expect(fixJson('-2.5e3'), '-2.5e3');
+        expect(fixJson('2.5E'), '2.5');
+        expect(fixJson('2.5E-'), '2.5');
+        expect(fixJson('2.5E3'), '2.5E3');
+        expect(fixJson('-2.5E3'), '-2.5E3');
+        expect(fixJson('12.e'), '12');
+        expect(fixJson('12.34e'), '12.34');
+        expect(fixJson('5e'), '5');
+      });
+
+      test('repairs incomplete strings and escapes', () {
+        expect(fixJson('"abc'), '"abc"');
+        expect(
+          fixJson(r'"value with \"quoted\" text and \\ escape'),
+          r'"value with \"quoted\" text and \\ escape"',
+        );
+        expect(fixJson(r'"value with \'), '"value with "');
+        expect(fixJson('"value with unicode <"'), '"value with unicode <"');
+      });
+
+      test('repairs arrays', () {
+        expect(fixJson('['), '[]');
+        expect(fixJson('[[1], [2'), '[[1], [2]]');
+        expect(fixJson('[["1"], ["2'), '[["1"], ["2"]]');
+        expect(fixJson('[[false], [nu'), '[[false], [null]]');
+        expect(fixJson('[[[]], [[]'), '[[[]], [[]]]');
+        expect(fixJson('[[{}], [{'), '[[{}], [{}]]');
+        expect(fixJson('[1, '), '[1]');
+        expect(fixJson('[[], 123'), '[[], 123]');
+      });
+
+      test('repairs objects', () {
+        expect(fixJson('{"key":'), '{}');
+        expect(
+          fixJson('{"a": {"b": 1}, "c": {"d": 2'),
+          '{"a": {"b": 1}, "c": {"d": 2}}',
+        );
+        expect(
+          fixJson('{"a": {"b": "1"}, "c": {"d": 2'),
+          '{"a": {"b": "1"}, "c": {"d": 2}}',
+        );
+        expect(
+          fixJson('{"a": {"b": false}, "c": {"d": 2'),
+          '{"a": {"b": false}, "c": {"d": 2}}',
+        );
+        expect(
+          fixJson('{"a": {"b": []}, "c": {"d": 2'),
+          '{"a": {"b": []}, "c": {"d": 2}}',
+        );
+        expect(
+          fixJson('{"a": {"b": {}}, "c": {"d": 2'),
+          '{"a": {"b": {}}, "c": {"d": 2}}',
+        );
+        expect(fixJson('{"ke'), '{}');
+        expect(fixJson('{"k1": 1, "k2'), '{"k1": 1}');
+        expect(fixJson('{"k1": 1, "k2":'), '{"k1": 1}');
+        expect(fixJson('{"key": "value"  '), '{"key": "value"}');
+        expect(fixJson('{"a": {"b": {}'), '{"a": {"b": {}}}');
+      });
+
+      test('repairs nested arrays and objects', () {
+        expect(fixJson('[1, [2, 3, ['), '[1, [2, 3, []]]');
+        expect(fixJson('[false, [true, ['), '[false, [true, []]]');
+        expect(fixJson('{"key": {"subKey":'), '{"key": {}}');
+        expect(
+          fixJson('{"key": 123, "key2": {"subKey":'),
+          '{"key": 123, "key2": {}}',
+        );
+        expect(
+          fixJson('{"key": null, "key2": {"subKey":'),
+          '{"key": null, "key2": {}}',
+        );
+        expect(fixJson('{"key": [1, 2, {'), '{"key": [1, 2, {}]}');
+        expect(
+          fixJson('[1, 2, {"key": "value",'),
+          '[1, 2, {"key": "value"}]',
+        );
+        expect(
+          fixJson('{"a": {"b": ["c", {"d": "e",'),
+          '{"a": {"b": ["c", {"d": "e"}]}}',
+        );
+        expect(
+          fixJson('{"a": {"b": {"c": {"d":'),
+          '{"a": {"b": {"c": {}}}}',
+        );
+        expect(fixJson('{"a": 1, "b": ['), '{"a": 1, "b": []}');
+        expect(fixJson('{"a": 1, "b": {'), '{"a": 1, "b": {}}');
+        expect(fixJson('{"a": 1, "b": "'), '{"a": 1, "b": ""}');
+      });
+
+      test('repairs regression cases', () {
+        expect(
+          fixJson(
+            [
+              '{',
+              '  "a": [',
+              '    {',
+              '      "a1": "v1",',
+              '      "a2": "v2",',
+              '      "a3": "v3"',
+              '    }',
+              '  ],',
+              '  "b": [',
+              '    {',
+              '      "b1": "n',
+            ].join('\n'),
+          ),
+          [
+            '{',
+            '  "a": [',
+            '    {',
+            '      "a1": "v1",',
+            '      "a2": "v2",',
+            '      "a3": "v3"',
+            '    }',
+            '  ],',
+            '  "b": [',
+            '    {',
+            '      "b1": "n"}]}',
+          ].join('\n'),
+        );
+        expect(
+          fixJson('{"type":"div","children":[{"type":"Card","props":{}'),
+          '{"type":"div","children":[{"type":"Card","props":{}}]}',
+        );
       });
     });
   });
